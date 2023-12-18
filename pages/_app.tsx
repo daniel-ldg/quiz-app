029adc2f4c793507402859b4732a9ea38c96069a
daniel-ldg <daniel.ldg@icloud.com>
2023-12-18 01:10:51 -0600

    replaced mantine by daisyui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,9 @@
 import "@mantine/core/styles.css";
 import type { AppProps } from "next/app";
-import { MantineProvider, createTheme } from "@mantine/core";
-
-const theme = createTheme({
-	/** Put your mantine theme override here */
-});
+import "../style/global.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
-	return (
-		<MantineProvider theme={theme}>
-			<Component {...pageProps} />
-		</MantineProvider>
-	);
+	return <Component {...pageProps} />;
 };
 
 export default App;
