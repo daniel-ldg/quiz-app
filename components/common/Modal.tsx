7b9cb6c5f772fb57b25b5c61f779661942f74441
daniel-ldg <daniel.ldg@icloud.com>
2023-12-20 01:56:04 -0600

    updated common component Modal
    added options to allow or deny closing

--- components/common/Modal.tsx
+++ components/common/Modal.tsx
@@ -1,40 +1,64 @@
 import { PropsWithChildren, useEffect, useRef, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { IoClose } from "react-icons/io5";
 
 interface Props extends PropsWithChildren {
 	isOpen: boolean;
 	onClose: () => void;
-	closeOnEsc?: boolean;
-	reRenderOnOpen?: boolean;
+	hideClose?: boolean;
+	ignoreEsc?: boolean;
+	ignoreBackdrop?: boolean;
+	keepChildMounted?: boolean;
 }
 
-const Modal: React.FC<Props> = ({ isOpen, onClose, closeOnEsc = false, reRenderOnOpen = false, children }) => {
+const Modal: React.FC<Props> = ({
+	isOpen,
+	onClose,
+	hideClose = false,
+	ignoreEsc = false,
+	ignoreBackdrop = false,
+	keepChildMounted = false,
+	children,
+}) => {
 	const ref = useRef<HTMLDialogElement>(null);
-	const [key, setKey] = useState(uuidv4());
 
 	useEffect(() => {
 		if (isOpen) {
 			ref.current?.showModal();
-			if (reRenderOnOpen) {
-				setKey(uuidv4());
-			}
 		} else {
 			ref.current?.close();
 		}
 	}, [isOpen]);
 
 	const handleEsc = (e: React.MouseEvent<HTMLDialogElement, MouseEvent>) => {
-		if (closeOnEsc) {
+		if (!ignoreEsc) {
 			onClose();
 		} else {
 			e.preventDefault();
 		}
 	};
 
+	const handleBackdropClick = (e: React.MouseEvent<HTMLDialogElement, MouseEvent>) => {
+		if (!ignoreBackdrop && e.target === ref.current) {
+			onClose();
+		}
+	};
+
 	return (
-		<dialog ref={ref} className="modal modal-bottom sm:modal-middle" onCancel={handleEsc}>
-			<div className="modal-box" key={key}>
-				{children}
+		<dialog
+			ref={ref}
+			className="modal modal-bottom sm:modal-middle"
+			onClick={handleBackdropClick}
+			onCancel={handleEsc}>
+			<div className="modal-box relative" onClick={e => e.stopPropagation()}>
+				{!hideClose && (
+					<button
+						className="absolute top-3 right-3 bg-transparent border-none text-black cursor-pointer"
+						onClick={onClose}
+						aria-label="Close">
+						<IoClose size={24} />
+					</button>
+				)}
+				{(isOpen || keepChildMounted) && children}
 			</div>
 		</dialog>
 	);
