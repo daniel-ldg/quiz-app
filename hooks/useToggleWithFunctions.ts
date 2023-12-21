56099d65cee896a11092df5bb0c78ed4a11a260d
daniel-ldg <daniel.ldg@icloud.com>
2023-12-21 04:28:09 -0600

    optimized hook by using useCallback

--- hooks/useToggleWithFunctions.ts
+++ hooks/useToggleWithFunctions.ts
@@ -1,27 +1,30 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { useToggle } from "./useToggle";
 
 export const useToggleWithFunctions = <T>(options: readonly T[]) => {
 	const [currentOption, toggle] = useToggle(options);
 	const [currentIndex, setCurrentIndex] = useState(0);
 
-	const setNextOption = () => {
+	const setNextOption = useCallback(() => {
 		setCurrentIndex(prevIndex => (prevIndex + 1) % options.length);
 		toggle(options[(currentIndex + 1) % options.length]);
-	};
+	}, [options, currentIndex, toggle]);
 
-	const setPrevOption = () => {
+	const setPrevOption = useCallback(() => {
 		setCurrentIndex(prevIndex => (prevIndex === 0 ? options.length - 1 : prevIndex - 1));
 		toggle(options[currentIndex === 0 ? options.length - 1 : currentIndex - 1]);
-	};
+	}, [options, currentIndex, toggle]);
 
-	const setOption = (option: T) => {
-		const newIndex = options.indexOf(option);
-		if (newIndex !== -1) {
-			setCurrentIndex(newIndex);
-			toggle(option);
-		}
-	};
+	const setOption = useCallback(
+		(option: T) => {
+			const newIndex = options.indexOf(option);
+			if (newIndex !== -1) {
+				setCurrentIndex(newIndex);
+				toggle(option);
+			}
+		},
+		[options, toggle]
+	);
 
 	return {
 		option: currentOption,
