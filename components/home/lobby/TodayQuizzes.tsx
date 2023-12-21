a0d5046adaf6e0eabdb674624c2316ec5658328d
daniel-ldg <daniel.ldg@icloud.com>
2023-12-21 04:32:56 -0600

    added revalidation overlay
    and now it revalidates data on mount

--- components/home/lobby/TodayQuizzes.tsx
+++ components/home/lobby/TodayQuizzes.tsx
@@ -1,5 +1,5 @@
 import useSWRImmutable from "swr/immutable";
-import React, { useEffect } from "react";
+import React from "react";
 import { GetTodayQuizzesResponse } from "@/pages/api/quiz/today";
 import { Fetcher } from "swr";
 
@@ -11,13 +11,13 @@ interface Props {
 const fetcher: Fetcher<GetTodayQuizzesResponse> = (url: string) => fetch(url).then(res => res.json());
 
 const TodayQuizzes: React.FC<Props> = ({ selected, onSelect }) => {
-	const { data, error, mutate, isLoading, isValidating } = useSWRImmutable("/api/quiz/today", fetcher);
+	const { data, error, mutate, isLoading, isValidating } = useSWRImmutable("/api/quiz/today", fetcher, {
+		revalidateOnMount: true,
+	});
 
 	const retryFetch = () => {
 		mutate();
 	};
-
-	useEffect(() => console.log("render"), []);
 
 	const loadingCard = <div className="p-4 m-2 rounded-lg bg-gray-100">Cargando...</div>;
 	const errorCard = (
@@ -35,7 +35,7 @@ const TodayQuizzes: React.FC<Props> = ({ selected, onSelect }) => {
 		<div
 			key={quiz.id}
 			onClick={() => onSelect(quiz.id)}
-			className={`p-2 m-2 rounded-lg cursor-pointer hover:bg-gray-100 ${
+			className={`p-2 m-2 rounded-lg cursor-pointer hover:bg-gray-100 border ${
 				selected === quiz.id ? "ring-2 ring-blue-500" : ""
 			}`}>
 			<h4 className="text-md font-semibold">{quiz.name}</h4>
@@ -43,12 +43,19 @@ const TodayQuizzes: React.FC<Props> = ({ selected, onSelect }) => {
 		</div>
 	));
 
+	const validatingOverlay = (
+		<div className="absolute w-full h-full bg-black/20 backdrop-blur-lg grid place-items-center">
+			<span className="loading loading-ring loading-lg"></span>
+		</div>
+	);
+
 	return (
-		<div className="flex flex-col max-h-96">
-			<h3 className="text-lg font-semibold">Selecciona un quiz</h3>
-			<div className="overflow-auto">
+		<div className="flex flex-col">
+			<h3 className="font-semibold">Selecciona un quiz</h3>
+			<div className={`relative h-28 ${isValidating ? "overflow-hidden" : "overflow-auto"}`}>
 				{error && errorCard}
-				{(isValidating || isLoading) && loadingCard}
+				{isLoading && loadingCard}
+				{isValidating && !isLoading && validatingOverlay}
 				{data?.quizzes.length === 0 ? noQuizzesCard : quizzesCards}
 			</div>
 		</div>
