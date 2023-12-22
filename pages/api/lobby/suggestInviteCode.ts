72192b486b2f7056a5276b2ecdd57824e405b23b
daniel-ldg <daniel.ldg@icloud.com>
2023-12-22 03:15:23 -0600

    added api handler suggestInviteCode
    generate random suggestion and check availability

---
+++ pages/api/lobby/suggestInviteCode.ts
@@ -0,0 +1,33 @@
+import { getRandomCode } from "@/utils/StringUtils";
+import { NextApiHandler } from "next";
+import prisma from "@/store/PrismaInstance";
+
+export interface SuggestInviteCodeResponse {
+	code: string;
+}
+
+const handler: NextApiHandler<SuggestInviteCodeResponse> = async ({ method }, res) => {
+	if (method !== "GET") {
+		res.setHeader("Allow", "GET");
+		res.status(405).end();
+		return;
+	}
+
+	let code: string;
+	let activeLobbiesWithSameCode: number;
+	do {
+		code = getRandomCode({ digits: 2 });
+		try {
+			activeLobbiesWithSameCode = await prisma.lobby.count({
+				where: { AND: { gameResult: { is: null }, inviteCode: { equals: code } } },
+			});
+		} catch (_) {
+			res.status(500).end();
+			return;
+		}
+	} while (activeLobbiesWithSameCode !== 0);
+
+	return res.json({ code });
+};
+
+export default handler;
