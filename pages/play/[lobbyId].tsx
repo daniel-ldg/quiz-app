340674bc5a7878d10665605c232c463d8e3910fb
daniel-ldg <daniel.ldg@icloud.com>
2023-12-30 01:02:45 -0600

    game page
    wraps game components in SocketContext
    set socket.io lobbyId query value
    returns 404 if game ended (for now, next: redirects to game result page)

---
+++ pages/play/[lobbyId].tsx
@@ -0,0 +1,36 @@
+import MainGame from "@/components/play/MainGame";
+import { SocketCtxProvider } from "@/context/SocketContext";
+import prisma from "@/store/PrismaInstance";
+import { Lobby } from "@prisma/client";
+import { GetServerSideProps } from "next";
+import isMongoId from "validator/lib/isMongoId";
+import { z } from "zod";
+
+interface Props {
+	lobbyId: Lobby["id"];
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async ({ query }) => {
+	try {
+		const parsedQuery = queryValidator.parse(query);
+		const { lobbyId } = parsedQuery;
+		const lobby = await prisma.lobby.findUniqueOrThrow({
+			where: { id: lobbyId, gameResult: { isSet: false } },
+			select: { id: true },
+		});
+		return { props: { lobbyId: lobby.id } };
+	} catch (_) {
+		return { notFound: true };
+	}
+};
+
+const Game: React.FC<Props> = ({ lobbyId }) => {
+	return (
+		<SocketCtxProvider lobbyId={lobbyId}>
+		</SocketCtxProvider>
+	);
+};
+
+const queryValidator = z.object({ lobbyId: z.string().refine(isMongoId) });
+
+export default Game;
